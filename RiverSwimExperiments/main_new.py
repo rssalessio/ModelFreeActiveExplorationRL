2c3e79eabacb10d67054d6ba4c79038963972ed5
Alessio Russo <alessior@kth.se>
2023-05-07 17:46:20 +0200

    more updates

--- RiverSwimExperiments/main_new.py
+++ RiverSwimExperiments/main_new.py
@@ -16,7 +16,86 @@ from agents.agent import AgentParameters, Experience
 from utils.utils import Results
 from make_agent import AgentType, make_agent
 import time
+from typing import NamedTuple, Sequence
+from numpy.typing import NDArray
+from utils.new_mdp_description import NewMDPDescription
 
+
+
+class DataResults(NamedTuple):
+    simulation_parameters: SimulationParameters
+    agent_type: AgentType
+    data: Sequence[Sequence[Results]]
+
+
+class SequencedResults(NamedTuple):
+    omega: NDArray[np.float64]
+    greedy_policy: NDArray[np.int64]
+    total_state_visits: NDArray[np.float64]
+    last_visit: NDArray[np.float64]
+    exp_visits: NDArray[np.float64]
+    eval_greedy: NDArray[np.float64]
+    dist_omega: NDArray[np.float64]
+    U_omega: NDArray[np.float64]
+    dist_value: NDArray[np.float64]
+    elapsed_times: NDArray[np.float64]
+    simulation_parameters: SimulationParameters
+
+def TV(p,q):
+    return np.sum(np.abs(p-q), -1) * 0.5
+
+def get_data(data: DataResults, true_mdp: NewMDPDescription, true_omega: NDArray[np.float64]) -> SequencedResults:
+    compute_dist_omega = lambda x,y: TV(x,y)
+    compute_dist_value = lambda V, mdp: np.linalg.norm(V -mdp.V_greedy[np.newaxis, np.newaxis], axis=-1)
+    data_omega = []
+    num_visits = []
+    policies = []
+    last_visit = []
+    visits = []
+    eval_greedy = []
+    elapsed_times = []
+    for i in range(data.simulation_parameters.n_sims):
+        _, _data_omega, _greedy_policy, _num_visits, _last_visit, _visits, _eval_greedy, _elapsed_times = zip(*data.data[i])
+        data_omega.append(_data_omega)
+        num_visits.append(_num_visits)
+        policies.append(_greedy_policy)
+        last_visit.append(_last_visit)
+        visits.append(_visits)
+        eval_greedy.append(_eval_greedy)
+        elapsed_times.append(_elapsed_times)
+    
+    data_omega = np.array(data_omega)
+    num_visits = np.array(num_visits)
+    policies = np.array(policies)
+    last_visit = np.array(last_visit)
+    visits = np.array(visits)
+    eval_greedy = np.array(eval_greedy)
+    elapsed_times = np.array(elapsed_times)
+
+    
+    omega = data_omega.reshape(data_omega.shape[0], data_omega.shape[1], -1)
+
+    dist_omega = compute_dist_omega(omega, true_omega.flatten()[np.newaxis, np.newaxis, ...])
+    U_omega = eval_allocations(omega, true_mdp, true_mdp.Q_greedy.shape[1])
+    dist_value = compute_dist_value(eval_greedy, true_mdp)
+    
+    return SequencedResults(data_omega, policies, num_visits, last_visit, visits, eval_greedy,
+                            dist_omega, U_omega, dist_value, elapsed_times, data.simulation_parameters)
+
+
+def eval_allocations(allocation, mdp, num_actions):
+    eval_x = np.zeros((allocation.shape[0], allocation.shape[1]))
+    for i in range(allocation.shape[0]):
+        for j in range(allocation.shape[1]):
+            eval_x[i,j] = mdp.evaluate_allocation(allocation[i,j].reshape(-1, num_actions))
+            if np.isinf(eval_x[i,j]):
+                eps = 1e-3
+                U = np.ones_like(allocation[i,j].reshape(-1, num_actions)) / np.prod(allocation[i,j].shape)
+                omega = (1-eps) * allocation[i,j].reshape(-1, num_actions) + eps * U
+                eval_x[i,j] = mdp.evaluate_allocation(omega)
+    return eval_x
+
+    
 def run(agent_type: AgentType, p: SimulationParameters):
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
@@ -74,19 +153,8 @@ if __name__ == '__main__':
         (10, EnvType.FORKED_RIVERSWIM, 50000),
         (10, EnvType.RIVERSWIM, 30000),
         (5, EnvType.FORKED_RIVERSWIM, 30000),
-        
-        
-        
-        
-        
-        
-        
         (5, EnvType.RIVERSWIM, 15000),
-        
         (3, EnvType.FORKED_RIVERSWIM, 15000),
-        
-        
-        
         (30, EnvType.RIVERSWIM, 100000),
         (15, EnvType.FORKED_RIVERSWIM, 100000)
     ]
@@ -100,8 +168,16 @@ if __name__ == '__main__':
          AgentType.BPI_NEW_BOUND_BAYES
         # AgentType.BPI_NEW_BOUND_BAYES,AgentType.PGOBPI,
     ]
-    agents = [AgentType.BPI_NEW_BOUND, AgentType.BAYESOBPI]
+    agents = [AgentType.OBPI]#, AgentType.BAYESOBPI]
     for length, env_type, horizon in types:
+        print(f'> Computing optimal allocation for {env_type.value}({length})... ')
+        if env_type == EnvType.FORKED_RIVERSWIM:
+            env = ForkedRiverSwim(length)
+        else:
+            env = RiverSwim(length)
+        mdp = NewMDPDescription(env.transitions, env.rewards, 0.99)
+        true_omega, _ = mdp.compute_allocation(navigation_constraints=True)
+            
         for agent in agents:
             print(f'> Evaluating {agent.value} on {env_type.value}({length})', end='... ')
     
@@ -133,6 +209,14 @@ if __name__ == '__main__':
 
             data['data'] = data_returned
             print(f'done in {np.round(time.time() - start_time, 2)} seconds.')
+            
+            
+            data = DataResults(data['simulation_parameters'], data['agent_type'], data['data'])
+            
+            data = get_data(data, mdp, true_omega)
+            
+            # import pdb
+            # pdb.set_trace()
             with lzma.open(f'{path}/{agent.value}_{length}.pkl.lzma', 'wb') as f:
                 pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
             
